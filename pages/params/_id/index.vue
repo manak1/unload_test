a113605f23e46f8b5d4afba07f6014189316e3f4
manak1 <mana23happy@hotmail.co.jp>
2020-12-04 18:16:19 +0900

    style: code unload sample

---
+++ pages/params/_id/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="pt-10">
+    <h2 class="text-4xl">Params使用パターン</h2>
+    <p class="text-2xl mt-5">現在のID {{ getCurrentId }}</p>
+    <div class="space-x-5 mt-10">
+      <a
+        href="#"
+        @click.prevent="previousPage"
+        class="inline-block rounded border-2 p-2 border-blue-200"
+      >
+        前のページへ
+      </a>
+      <a
+        href="#"
+        @click.prevent="nextPage"
+        class="inline-block rounded border-2 p-2 border-blue-200"
+      >
+        次のページへ
+      </a>
+    </div>
+    <div class="mt-10 space-x-5">
+      <nuxt-link to="/" class="text-blue-700 underline">
+        トップへ戻る(nuxt-linkタグ)
+      </nuxt-link>
+
+      <a href="/" class="text-blue-700 underline">
+        トップへ戻る(aタグ)
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.addEventListener("beforeunload", this.confirm);
+  },
+  methods: {
+    confirm(event) {
+      event.preventDefault();
+
+      // IEのみメッセージを変更できます。
+      event.returnValue = "";
+    },
+
+    nextPage() {
+      this.$router.push(`/params/${Number(this.getCurrentId) + 1}`);
+    },
+
+    previousPage() {
+      if (this.getCurrentId > 0) {
+        this.$router.push(`/params/${Number(this.getCurrentId) - 1}`);
+      }
+    },
+    
+    confirm(event) {
+      event.preventDefault()
+
+      // IEのみメッセージを変更可能。
+      event.returnValue = ""
+    }
+  },
+  mounted() {
+    window.addEventListener("beforeunload", this.confirm);
+  },
+
+  computed: {
+    getCurrentId() {
+      return this.$route.params.id;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/query/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="pt-10">
+    <h2 class="text-4xl">Query使用パターン</h2>
+    <p class="text-2xl mt-5">現在のページ番号 {{getCurrentPage}}</p>
+    <div class="space-x-５ mt-10">
+      <a href="#" @click.prevent="previousPage" class="inline-block rounded border-2 p-2 border-blue-200" >
+        前のページへ
+      </a>
+      <a href="#" @click.prevent="nextPage" class="inline-block rounded border-2 p-2 border-blue-200">
+        次のページへ
+      </a>
+    </div>
+    <div class="mt-10 space-x-5">
+      <nuxt-link to="/" class="text-blue-700 underline">
+        トップへ戻る(nuxt-linkタグ)
+      </nuxt-link>
+
+      <a href="/" class="text-blue-700 underline">
+        トップへ戻る(aタグ)
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.addEventListener("beforeunload", this.confirm);
+
+    //pageクエリーが1の時、pushが重複して起こらないように確認。
+    if (this.$route.query.page != 1) {
+      this.$router.replace("?page=1");
+    }
+  },
+  methods: {
+    confirm(event) {
+      event.preventDefault();
+
+      // IEのみメッセージを変更可能。
+      event.returnValue = "";
+    },
+
+    nextPage() {
+      this.$router.push(`?page=${Number(this.getCurrentPage) + 1}`)
+    },
+
+    previousPage() {
+      if(this.getCurrentPage > 0) {
+        this.$router.push(`?page=${Number(this.getCurrentPage)-1}`)
+      }
+    }
+  },
+
+  computed: {
+    getCurrentPage() {
+      return this.$route.query.page 
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
